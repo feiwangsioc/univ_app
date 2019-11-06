39562b7661957b66961587b6ea261ba4c6dc1741
fei wang <feiwangsioc@gmail.com>
2019-11-06 03:32:57 +0000

    students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 class Student < ApplicationRecord
+  
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :email, presence: true, length: { minimum: 5, maximum: 15 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
+  validates :email, presence: true, length: { maximum: 105 },
+                                    uniqueness: {case_sensitive: false},
+                                    format: {with: VALID_EMAIL_REGEX }
+  
+  
 end 
