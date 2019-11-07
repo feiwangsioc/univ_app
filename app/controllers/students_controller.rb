19d67b1177957c51b1c60d1e4acaf002e528e525
fei wang <feiwangsioc@gmail.com>
2019-11-07 03:36:39 +0000

    validate

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,22 @@ class StudentsController < ApplicationController
   end 
   
   def new
-  @student = Student.new  
+    @student = Student.new  
   end 
   
+  def create 
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end 
+  
+  end 
+  
+  private
+   def student_params
+     params.require(:student).permit(:name, :email)
+   end 
 end 
